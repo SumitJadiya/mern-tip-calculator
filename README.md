ff4ba237d55c3bec538149805390243cf74f0f3c
sumit.jadiya <sumit.jadiya@appdirect.com>
2020-09-16 12:19:32 +0530

    add css

---
+++ README.md
@@ -0,0 +1,7 @@
+### This app Uses MERN stack.
+
+<br/>
+> M-> Mongo<br/>
+> E-> Express<br/>
+> R -> React<br/>
+> N -> Node<br/>
--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import { useState } from 'react';
+import { Form, Button, Col, InputGroup, Container } from 'react-bootstrap';
+
 
 function App() {
   const [amount, setAmount] = useState('')
@@ -24,20 +26,49 @@ function App() {
     }).then((res) => {
       return res.json()
     }).then((data) => {
-      setTip(data.toBePayed)
+      setTip(`Total Amount after Tip is -> ₹. ${data.toBePayed}/-`)
+      setAmount('')
+      setPercentage('')
     })
   }
 
   return (
     <div className="App">
-      {/* Amount */}
-      <input type="text" placeholder="Please add the Amount" value={amount} onChange={e => setAmount(e.target.value)} />
-      {/* Percentage */}
-      <input type="text" placeholder="Please add the percentage" value={percentage} onChange={e => setPercentage(e.target.value)} />
-      {/* Button */}
-      <button onClick={calculateTip}>Calculate My Tip</button>
-      {/* Output */}
-      <h1>{tip}</h1>
+      <Container>
+        <Form style={{ border: '1px solid #999', padding: '10px', marginTop: '20px' }}>
+          <Form.Row className="align-items-center" style={{ marginTop: '20px' }}>
+            <Col>
+              <InputGroup>
+                <InputGroup.Prepend>
+                  <InputGroup.Text>Amount</InputGroup.Text>
+                </InputGroup.Prepend>
+                {/* Amount */}
+                <Form.Control type="text" placeholder="Please add the Amount" value={amount} onChange={e => setAmount(e.target.value)} autoFocus />
+                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              </InputGroup>
+            </Col>
+          </Form.Row>
+          <Form.Row style={{ marginTop: '20px' }}>
+            <Col>
+              {/* Percentage */}
+              <InputGroup>
+                <InputGroup.Prepend>
+                  <InputGroup.Text>Tip %</InputGroup.Text>
+                </InputGroup.Prepend>
+                <Form.Control type="text" placeholder="Please add the percentage" value={percentage} onChange={e => setPercentage(e.target.value)} />
+                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              </InputGroup>
+            </Col>
+          </Form.Row>
+          <Form.Row style={{ textAlign: 'center', display: 'block', marginTop: '20px' }}>
+            {/* Button */}
+            <Button onClick={calculateTip}>Calculate My Tip</Button>
+            {/* Output */}
+            <br />
+            <h1 style={{ marginTop: '20px' }}>{tip}</h1>
+          </Form.Row>
+        </Form>
+      </Container>
     </div>
   );
 }
